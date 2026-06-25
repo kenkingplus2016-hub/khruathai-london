f51c6fad9a78eef97671edcc936d862566d4164b
kenkingplus2016-hub <kenkingplus2016@gmail.com>
2026-06-25 16:55:44 +0100

    Update

--- check_logo_dims.js
+++
@@ -1,8 +0,0 @@
-const Jimp = require('jimp');
-
-async function checkLogo() {
-    const logo = await Jimp.read("c:\\Users\\KENDEE\\Desktop\\เว็บ\\public\\logo.png");
-    console.log(`Cropped logo dimensions: Width: ${logo.bitmap.width}, Height: ${logo.bitmap.height}`);
-}
-
-checkLogo().catch(console.error);
