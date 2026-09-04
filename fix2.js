97d690cac6bd38ec31844249a3d27d4168e36dfc
kenkingplus2016-hub <kenkingplus2016@gmail.com>
2026-09-04 17:42:09 +0100

    1

--- fix2.js
+++ fix2.js
@@ -1,5 +1,5 @@
-﻿const fs = require('fs');
+const fs = require('fs');
 let html = fs.readFileSync('public/meeting-meals.html', 'utf8');
 html = html.replace(/Premium Coffee &amp; Tea, served with Assorted Sandwiches or Thai Sangkhaya Bread \([^)]+\)/g, 'Premium Coffee &amp; Tea, served with Assorted Sandwiches or Thai Sangkhaya Bread (แซนด์วิช หรือ ขนมปังสังขยา)');
 fs.writeFileSync('public/meeting-meals.html', html, 'utf8');
-console.log('Fixed using Node');
+console.log('Fixed using Node write_to_file');
